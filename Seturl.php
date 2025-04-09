2b87801a4fbc5e64ef16ebaf3dcd95856d48c114
Pankajji1 <pankajkumerji1@gmail.com>
2025-04-09 22:53:41 +0530

    Update Seturl.php

--- Seturl.php
+++ Seturl.php
@@ -3,5 +3,5 @@
 $password = "mypassword";
 $hash = password_hash($password, PASSWORD_BCRYPT);
 file_put_contents("key", $hash);
-$expiryTime = 21600; // 6 घंटे (21600 सेकंड)
+$expiryTime = 21600000000000; // 6 घंटे (21600 सेकंड)
 $file = "key";
